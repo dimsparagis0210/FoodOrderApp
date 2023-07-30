60977f87984ef01a5ed6ffd7cbc3aa52020ffc35
dimsparagis0210 <dimsparagis@gmail.com>
2023-07-30 18:38:43 +0300

    Initialize project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import classes from './Header.module.css';
+
+export const Header = () => {
+    return (
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+        </header>
+    );
+}
