ee01cdedcf32cd55f055dd680e4d51ec4884dcc6
dimsparagis0210 <dimsparagis@gmail.com>
2023-08-07 17:16:36 +0300

    Added toolbar

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,17 @@
+import classes from './Header.module.css';
+import meals from '../../../assets/meals.jpg';
+import {HeaderCartButton} from "../HeaderCartButton/HeaderCartButton";
+
+export const Header = props => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={meals} alt="Meals"/>
+            </div>
+        </>
+    );
+}
