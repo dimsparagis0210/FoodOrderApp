1f9dbf093c4a86494d841f88a8fb191c7fa9c226
dimsparagis0210 <dimsparagis@gmail.com>
2023-08-07 18:03:36 +0300

    Added Cart modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Header } from "./components/Layout/Header/Header";
+import {Header} from "./components/Layout/Header/Header";
 import {Meals} from "./components/Meals/Meals";
+import {Cart} from "./components/Cart/Cart";
 
 function App() {
-  return (
-      <>
-        <Header />
-        <main>
-          <Meals />
-        </main>
-      </>
-  );
+    return (
+        <>
+            <Cart></Cart>
+            <Header/>
+            <main>
+                <Meals/>
+            </main>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,19 @@
+import classes from './Cart.module.css';
+import { Modal } from "../UI/Modal";
+
+export const Cart = props => {
+    const cartItems = [{id: 'c1', name: 'sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>);
+    return <Modal>
+        <ul className={classes['cart-items']}>
+            {cartItems}
+        </ul>
+        <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>35.62</span>
+        </div>
+        <div className={classes.actions}>
+            <button className={classes['button--alt']}>Order</button>
+            <button className={classes.button}>Close</button>
+        </div>
+    </Modal>
+};
