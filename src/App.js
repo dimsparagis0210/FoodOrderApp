e23f00a2cdd6153ca7a6194d0ea9bd9bbbdfe874
dimsparagis0210 <dimsparagis@gmail.com>
2023-08-09 17:58:42 +0300

    Added context for the cart content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import {Header} from "./components/Layout/Header/Header";
-import {Meals} from "./components/Meals/Meals";
-import {Cart} from "./components/Cart/Cart";
+import { Header } from "./components/Layout/Header/Header";
+import { Meals } from "./components/Meals/Meals";
+import { Cart } from "./components/Cart/Cart";
 import { useState } from 'react';
+import { CartProvider } from "./store/CartProvider";
 
 function App() {
     const [cartIsShown, setCartIsShown] = useState(false);
@@ -14,13 +15,13 @@ function App() {
         setCartIsShown(false);
     }
     return (
-        <>
-            {cartIsShown && <Cart onClose={hideCartHandler} />};
+        <CartProvider>
+            {cartIsShown && <Cart onClose={hideCartHandler}/>};
             <Header onShowCart={showCartHandler} />
             <main>
                 <Meals/>
             </main>
-        </>
+        </CartProvider>
     );
 }
 
--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,14 +1,42 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from "../../Cart/CartIcon";
+import { useContext, useEffect, useState } from 'react';
+import { CartContext } from "../../../store/cart-context";
 
 export const HeaderCartButton = props => {
+
+    const cartCtx = useContext(CartContext);
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const {items} = cartCtx;
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ' '}`;
+
+    const numberOfCartItems = items.reduce((currNumber, item) => {
+        return currNumber + item.amount;
+    }, 0);
+
+    useEffect(() => {
+        if(items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        //Cleanup function
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
